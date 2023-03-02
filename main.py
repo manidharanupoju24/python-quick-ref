3d6b61f5bc4cd55e59ade278dce241dbcf0a2ea6
uchihaitachi24 <manidharanupoju@gmail.com>
2023-03-02 17:12:20 -0600

    added dictionaries code

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-calculation_to_units = 24 * 60 * 60
-name_of_unit = "seconds"
+calculation_to_hours = 24
+calculation_to_minutes = calculation_to_hours * 60
+calculation_to_seconds = calculation_to_minutes * 60
 
 
-def days_to_units(num_of_days):
+def days_to_units(num_of_days, conversion_unit):
     # print(type(num_of_days > 0))
-    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+    if conversion_unit == "hours":
+        return f"{num_of_days} days are {num_of_days * calculation_to_hours} {conversion_unit}"
+    elif conversion_unit == "seconds":
+        return f"{num_of_days} days are {num_of_days * calculation_to_seconds} {conversion_unit}"
+    elif conversion_unit == "minutes":
+        return f"{num_of_days} days are {num_of_days * calculation_to_minutes} {conversion_unit}"
+    else:
+        return "Unsupported Unit"
 
 
 def validate_and_execute():
      #if user_input.isdigit():
     try :
-        user_input_number = int(num_of_days_element)
+        user_input_number = int(days_and_unit_dictionary["days"])
         if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number)
+            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
             print(calculated_value)
         elif user_input_number == 0 :
             print("You entered 0 days. Enter atleast one positive number to calculate.")
@@ -25,11 +33,20 @@ def validate_and_execute():
 
 user_input = ""
 while user_input != "exit":
-    user_input = input("Enter the no of days you want to convert:\n")
+    user_input = input("Enter the no of days you want to convert, along with the conversion unit:\n")
     # print(type(user_input.split(", ")))
     # print(user_input.split(", "))
-    for num_of_days_element in set(user_input.split(", ")):
+    list_of_days = user_input.split(":") # splits into 2 values into a list
+    print(list_of_days)
+    days_and_unit_dictionary = {"days": list_of_days[0], "unit": list_of_days[1]}
+    print(days_and_unit_dictionary)
+    print(type(days_and_unit_dictionary))
+    validate_and_execute()
+    """
+      for num_of_days_element in set(list_of_days):
         validate_and_execute()
+    """
+
 
 
 #print("20 days are " + str(20 * 24 * 60) + " minutes")
@@ -51,4 +68,15 @@ input() = asks user for input
 split() = takes in a special char and splits the text from it 
 set() = returns a new set for. e.g. converts a list to int
 *** each datatype has its own set of built in functions ***
+"""
+
+"""
+datatypes : 
+Integers
+strings
+float
+boolean
+lists
+sets
+dictionaries
 """
